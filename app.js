21d0d37dad026aaa65a21aa6c45794e614fb217f
SokolovPavel7 <sokolovpavel7@gmail.com>
2022-12-29 20:52:56 +0300

    add javascript file

---
+++ app.js
@@ -0,0 +1,19 @@
+const cols = document.querySelectorAll('.col');
+
+function generateRandomColor() {
+  const hexCodes =
+    '0123456789ABCDEF'; /*16 символов, которые используются в генерации цвета*/
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return '#' + color;
+}
+
+function setRandomColors() {
+  cols.forEach((col) => {
+    col.style.background = generateRandomColor();
+  });
+}
+
+setRandomColors();
